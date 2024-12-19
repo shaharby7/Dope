5dd9c2968344a8884545fab9fc65df470ddab4a1
shahar <shahar@hyperguest.com>
2024-12-19 22:12:35 +0200

    feat(install): basic install cli toll

---
+++ pkg/cli/cmd/install.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/shaharby7/Dope/pkg/install"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	cmdRoot.AddCommand(cmdInstall)
+}
+
+var cmdInstall = &cobra.Command{
+	Use:   "install",
+	Short: "install the project",
+	Long:  `install the dope essential helm chart. Thin wrapper for the command helm install dope dope/dope -n dope -f <dst>/build/helm/local/dope/values.yaml --create-namespace`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := install.InstallProject(
+			viper.GetString(string(CONF_VARS_DST)),
+			&install.InstallOptions{},
+		)
+		return err
+	},
+}
